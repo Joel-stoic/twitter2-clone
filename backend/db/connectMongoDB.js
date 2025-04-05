f0def6e0e6647b01ebc1963461d0aa6ae8f317c7
Joel-stoic <joelimmanual.off@gmail.com>
2025-04-06 02:37:39 +0530

    1.user routes and controllers added

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 
 const connectMongoDB=async()=>{
     try {
-        const conn=await mongoose.connect(process.env.MONGO_URI)
+        const conn=await mongoose.connect(process.env.MONGO_URI,{
+            serverSelectionTimeoutMS: 60000,
+            connectTimeoutMS: 60000,
+            socketTimeoutMS: 60000
+        })
         console.log(`MongoDB connected:${conn.connection.host}`)
     } catch (error) {
         console.log(`Error connection to mongoDB: ${error.message}`)
